d14565e56424a12ff5c7a1f7be1a9950e4c31a7a
dann9109 <danielxstella@gmail.com>
2024-02-16 20:09:05 -0500

    controllers

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,29 +1,116 @@
 const { User, Thought } = require("../models")
 
 
+async function createUser(req, res) {
+    try {
+        const user = await User.create(req.body);
+
+        res.json(user)
+    } catch (err) {
+        console.log(err);
+    }
+}
 
 async function getAllusers(req, res) {
-    const allUsers = await User.find()
-    res.json(allUsers)
+    try {
+        const users = await User.find();
+
+        res.json(users);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function getUserbyId(req, res) {
+    try {
+        const user = await User.findById(req.params.user_id);
+
+        if (!user) return res.status(404).json({
+            message: 'User with ID not found'
+        });
+
+        res.json(user);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function updateUser(req, res) {
+    try {
+        if (email) {
+            const user = await User.findByIdAndUpdate(req.params.user_id, {
+                $set: {
+                    email: email
+                }
+            }, { new: true });
+
+            res.json(user);
+        }
+
+        if (password) {
+            const user = await User.findById(req.params.user_id);
+
+            if (!user) return res.status(404).json({
+                message: 'User with ID not found'
+            });
+
+            // Check if old password is correct
+            const pass_valid = await user.validatePass(password);
+
+            if (!pass_valid) return res.status(401).json({
+                message: 'The old password is incorrect'
+            });
+
+            user.password = newPassword;
+
+            user.save();
+
+            res.json(user);
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+async function deleteUser(req, res) {
+    try {
+        await User.deleteOne({ _id: req.params.user_id });
+
+        res.json({
+            message: 'User deleted successfully!'
+        });
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 async function addFriend(req, res) {
-    await this.updateOne(
-        { _id: userId },
-        { $addToSet: { friends: friendId } }
-    );
+    try {
+        await User.updateOne(
+            { _id: userId },
+            { $addToSet: { friends: friendId } }
+        );
+    } catch (err) {
+        console.log(err);
+    }
+
 }
 
 async function removeFriend(req, res) {
-    await this.updateOne(
-        { _id: userId },
-        { $pull: { friends: friendId } }
-    );
+    try {
+        await User.updateOne(
+            { _id: userId },
+            { $pull: { friends: friendId } }
+        );
+    } catch (err) {
+        console.log(err);
+    }
 }
 
 
 module.exports = {
-    getAllusers, addFriend, removeFriend
+    getAllusers, addFriend, removeFriend, deleteUser, updateUser, getUserbyId, createUser
 }
 
 
--- routes/api/user_routes.js
+++ routes/api/user_routes.js
@@ -1,119 +1,25 @@
 const router = require('express').Router();
 
-const { User } = require('../../models/');
-const { getAllusers, addFriend, removeFriend } = require('../../controllers/user_controller')
 
-// Create a user
-router.post('/users', async (req, res) => {
-    try {
-        const user = await User.create(req.body);
+const { getAllusers, addFriend, removeFriend, createUser, getUserbyId, updateUser, deleteUser } = require('../../controllers/user_controller')
 
-        res.json(user);
-    } catch (err) {
-        console.log(err);
-    }
-});
+
+
+
 // /api/users/
-// Get all users
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find();
+// Get all users// Create a user
+router.route("/").get(getAllusers).post(createUser);
 
-        res.json(users);
-    } catch (err) {
-        console.log(err);
-    }
-});
+// Get one user by ID // Update user //Delete User
+router.route('/:user_id').get(getUserbyId).put(updateUser).delete(deleteUser)
 
-// Get one user by ID
-router.get('/users/:user_id', async (req, res) => {
-    try {
-        const user = await User.findById(req.params.user_id);
 
-        if (!user) return res.status(404).json({
-            message: 'User with ID not found'
-        });
 
-        res.json(user);
-    } catch (err) {
-        console.log(err);
-    }
-});
-
-// Update a single user
-router.put('/users/:user_id', async (req, res) => {
-    const { email, password, newPassword } = req.body;
-
-    try {
-        if (email) {
-            const user = await User.findByIdAndUpdate(req.params.user_id, {
-                $set: {
-                    email: email
-                }
-            }, { new: true });
-
-            res.json(user);
-        }
-
-        if (password) {
-            const user = await User.findById(req.params.user_id);
-
-            if (!user) return res.status(404).json({
-                message: 'User with ID not found'
-            });
-
-            // Check if old password is correct
-            const pass_valid = await user.validatePass(password);
-
-            if (!pass_valid) return res.status(401).json({
-                message: 'The old password is incorrect'
-            });
-
-            user.password = newPassword;
-
-            user.save();
-
-            res.json(user);
-        }
-
-    } catch (err) {
-        console.log(err);
-    }
-});
-
-// Delete a user
-router.delete('/users/:user_id', async (req, res) => {
-    try {
-        await User.deleteOne({ _id: req.params.user_id });
-
-        res.json({
-            message: 'User deleted successfully!'
-        });
-    } catch (err) {
-        console.log(err);
-    }
-});
 
 // Add a friend to a user's friend list
-router.post('/users/:userId/friends/:friendId', async (req, res) => {
-    try {
-        await User.addFriend({ _id: req.params.friendId });
-        res.json({ message: 'Friend added successfully' });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
+router.route.post('/:userId/friends/:friendId').post(addFriend)
 
 // Remove a friend from a user's friend list
-router.delete('/users/:userId/friends/:friendId', async (req, res) => {
-    try {
-        await User.removeFriend({ _id: req.params.friendId });
-        res.json({ message: 'Friend removed successfully' });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
+router.route.delete(':userId/friends/:friendId').delete(removeFriend)
 
 module.exports = router;
